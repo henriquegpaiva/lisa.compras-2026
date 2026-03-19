d0f9b419d861167ff871984ad12553c4313a096d
henriquegpaiva <goncalves.paiva@escola.pr.gov.br>
2026-03-19 10:43:46 -0300

    Faz o L

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { supabase } from './supabaseClient.js'
 
-// Recupera o usuÃ¡rio logado. Redireciona para login se nÃ£o estiver autenticado.
+// Recupera o usuário logado. Redireciona para login se não estiver autenticado.
 async function getUser() {
   const { data: { user } } = await supabase.auth.getUser()
   if (!user) window.location.href = 'login.html'
@@ -36,7 +36,7 @@ window.adicionarItem = async function () {
 
   const { error } = await supabase.from('lista_compras').insert({
     item: input.value,
-    adicionada_por: user.id // Certifique-se que a coluna existe e Ã© do tipo uuid
+    adicionado_por: user.id // Certifique-se que a coluna existe e Ã© do tipo uuid
   })
 
   if (error) return alert('Erro ao adicionar: ' + error.message)
@@ -64,6 +64,6 @@ getUser().then(carregarLista)
 // enter funciona para adicionar item
 document.addEventListener('keydown', function (event) {
   if (event.key === 'Enter') {
-      adicionarItem()
- }
+    adicionarItem()
+  }
 })
--- auth.js
+++ auth.js
@@ -24,7 +24,7 @@ window.cadastro = async function () {
   if (error) {
     alert('Erro no cadastro: ' + error.message)
   } else {
-    alert('Cadastro realizado! Faça login.')
+    alert('Cadastro realizado! Confirme o seu E-mail.')
   }
 }
 
